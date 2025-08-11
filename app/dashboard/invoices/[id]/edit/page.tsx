5852e6200fbce7e51da352c3735c50d2c1ebdff0
eddy rodas lima <eddylima@modalai.com>
2025-08-10 20:46:34 -0700

    new updaes from window

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,15 +1,19 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-
-
+import { notFound } from 'next/navigation';
+ 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
-  const [invoice, customers] = await Promise.all([
+   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+   if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
@@ -21,7 +25,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
             href: `/dashboard/invoices/${id}/edit`,
             active: true,
           },
-        ]}
+      ]}
       />
       <Form invoice={invoice} customers={customers} />
     </main>
